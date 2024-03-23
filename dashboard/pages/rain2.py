be4f81b1dcbfaa3b16898de2c5c0d500fe98e37c
Gustyper <gu.bianchi.s@gmail.com>
2024-03-23 11:42:29 -0300

    Criando gráfico de alagamentos por bairro

--- dashboard/pages/rain2.py
+++ dashboard/pages/rain2.py
@@ -7,8 +7,13 @@ from components.header import header
 from components.apply import apply_updates
 from components.container import create_container_graph
 
+from google.cloud import bigquery
+
 # Registrando a página
 dash.register_page(__name__, path="/rain2", name="Chuva2")
+
+# Crie uma instância do cliente BigQuery
+client = bigquery.Client(project='hackaton-fgv-guris')
 
 # Função de callback para armazenar o DataFrame
 @callback(
@@ -24,14 +29,9 @@ def store_data(id):
     # SELECT * FROM `rj-rioaguas.saneamento_drenagem.ponto_supervisionado_alagamento`
     # """
     # query_job = client.query(query)
+    # df = query_job.to_dataframe()
 
-    # df_alertario = query_job.to_dataframe()
-
-    frequencia = dict(df.value_counts(df["bairro"])) # Convertendo o DataFrame para um dicionário
-
-    filtered_dict = {k: v for k, v in frequencia.items() if v >= 5}
-
-    return filtered_dict  
+    return df
 
 # Função de callback para atualizar o gráfico de pizza
 @callback(
@@ -39,11 +39,15 @@ def store_data(id):
     Input('alagamento', 'data')
 )
 def update_column_graph(data):
-    
-    locations = list(data.keys())
-    values = list(data.values())
 
-    # Create a bar chart
+    # Convertendo o DataFrame para um dicionário
+    frequencia = dict(data.value_counts(data["bairro"]))
+    filtered_dict = {k: v for k, v in frequencia.items() if v >= 5}
+    
+    locations = list(filtered_dict.keys())
+    values = list(filtered_dict.values())
+
+    # Cria um gráfico de barra
     fig = px.bar(x=locations, y=values, title="Sales by Location")
 
     fig = apply_updates(fig)
@@ -55,7 +59,9 @@ def update_column_graph(data):
     Input('alagamento', 'data')
 )
 def update_line_chart(data):
+
     df = pd.DataFrame(data)
+
     fig = px.line(df, x='Cidade', y='Chuva', title='Chuva no Rio de Janeiro')
     
     fig = apply_updates(fig)
@@ -96,7 +102,7 @@ layout = html.Div([
     header("Chuva no Rio de Janeiro"),
     
     html.Div([
-        create_container_graph('rain-graph-1B', "Gráfico 1"),
+        create_container_graph('rain-graph-1B', "Casos de Alagamento por Bairro"),
         create_container_graph('rain-graph-2B', "Gráfico 2"),
         create_container_graph('rain-graph-3B', "Gráfico 3"),
         create_container_graph('rain-graph-4B', "Gráfico 4"),
